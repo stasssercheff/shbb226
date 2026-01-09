538bd99562428ab4aaded6b98bc428558ceaab2c
stasssercheff <stassserchef@gmail.com>
2026-01-09 08:46:15 +0700

    Update goHome function to redirect to site root

--- kitchen/kitchen/checklist/script.js
+++ kitchen/kitchen/checklist/script.js
@@ -14,7 +14,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // На главную
 function goHome() {
-    location.href = "http://stasssercheff.github.io/shbb226/";
+  // Переходим в корень сайта
+  location.href = location.origin + location.pathname.split('/')[1] + '/';
 }
 
 // На уровень выше (одну папку вверх)
