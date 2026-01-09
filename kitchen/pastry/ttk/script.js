61084753bad02b812b8ef802345173461a3fafef
stasssercheff <stassserchef@gmail.com>
2026-01-09 08:51:02 +0700

    Update script.js

--- kitchen/pastry/ttk/script.js
+++ kitchen/pastry/ttk/script.js
@@ -1,7 +1,8 @@
 
 // На главную
 function goHome() {
-    location.href = "http://stasssercheff.github.io/shbb25/";
+  // Переходим в корень сайта
+  location.href = location.origin + location.pathname.split('/')[1] + '/';
 }
 
 // На уровень выше (одну папку вверх)
